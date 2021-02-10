46cb17945efefd2c0a6be4ab74818780fab28d56
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-02-09 23:02:29 -0600

    Added the basic CSS and UI

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', main);
+
+function main() {
+    console.log('Page is fully loaded');
+}
+
+async function onClickColor1() {
+
+}
+
+async function onClickColor2() {
+    
+}
