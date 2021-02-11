66ce5499e115f2423a2129f5aa12a15331bae45d
Bhaskar <reddybhaskar437@gmail.com>
2021-02-11 16:22:51 -0600

    Added speech synthesis to the buttons

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,13 @@ function main() {
 }
 
 async function onClickColor1() {
+    let utterance = new SpeechSynthesisUtterance("Color 1 is Orange");
+speechSynthesis.speak(utterance);
 
 }
 
 async function onClickColor2() {
-    
+    let utterance = new SpeechSynthesisUtterance("Color 2 is Blue");
+speechSynthesis.speak(utterance); 
+
 }
